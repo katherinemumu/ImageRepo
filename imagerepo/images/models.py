3aa14b79f8c90db3a17c249b7ed9b5a8a01c2981
Katherine Mu <katherinemu47@gmail.com>
2021-05-09 17:55:12 -0400

    can view images in a grid on index page

--- imagerepo/images/models.py
+++ imagerepo/images/models.py
@@ -5,8 +5,8 @@ from django.db import models
 class Image(models.Model):
     title = models.CharField(max_length=200)
     # date_uploaded = models.DateTimeField('date_uploaded')
-    tag = models.CharField(max_length=200)
+    tag = models.CharField(max_length=200, blank=True)
     image = models.ImageField(upload_to='', default='default.jpg')
 
     def __str__(self):
-        return self.title
+        return self.title + ' ' + self.image.name
--- imagerepo/images/views.py
+++ imagerepo/images/views.py
@@ -2,10 +2,31 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .form import ImageForm
+from .models import Image
 from datetime import datetime
 
 def index(request):
     template = loader.get_template('images/index.html')
+    pics = Image.objects.all()
+    print('++++++++++++++++++' + str(type(pics)) + "+++++ " + str(pics))
+    length = len(pics)
+    i = 0
+    array = []
+    sub_array =[]
+    for pic in pics:
+        img_path = "../../../media/" + str(pic.image)
+        print(img_path)
+        sub_array.append(img_path)
+        if len(sub_array) == 3:
+            array.append(sub_array)
+            sub_array = []
+    if len(sub_array) < 3:
+        array.append(sub_array)
+    print(array)
+    return render(request, 'images/index.html', {'array': array})
+    # "../../../media/pusheeen.jpg"
+
+
     return HttpResponse(template.render({}, request))
 
 def upload(request):
